56477717f7b23d383d9035f5cd993b8ef457deaa
KS <khyati.a.shah@gmail.com>
2026-04-07 22:33:38 -0700

    solved serialize and deserialize b-tree

--- leetcode_297_serialize_deserialize_btree.py
+++ leetcode_297_serialize_deserialize_btree.py
@@ -6,15 +6,23 @@ class Codec:
     def serialize(self, root: TreeNode):
         if not root:
             return ""
-        def inOrderTraverse (node: TreeNode):
-            if node is None:
-                return "#"
-            if not (node.left or node.right):
-                return f"{node.val}"
-            if not node.right:
-                return f"{node.val},{inOrderTraverse(node.left)}"
-            return f"{node.val},{inOrderTraverse(node.left)},{inOrderTraverse(node.right)}"
-        return inOrderTraverse(root)
+        result = ""
+        queue = [root]
+        nullNode = TreeNode("#")
+        while queue:
+            x = queue.pop(0)
+            result = f"{result},{x.val}"
+            if x != nullNode:
+                if x.left:
+                    queue.append(x.left)
+                else:
+                    queue.append(nullNode)
+                if x.right:
+                    queue.append(x.right)
+                else:
+                    queue.append(nullNode)
+        #NOTE: Leaf nodes result in trailing and leading extra characters. Strip them
+        return result.rstrip(",#").lstrip(",")
 
     ## returns tree representation from string
     ##NOTE: to have string data type, use str, not string
@@ -22,55 +30,51 @@ class Codec:
         if data == "":
             return []
         node_array = data.split(",")
-        queue = []
-        x= node_array.pop(0)
-        queue.append (x)
-        root = None
+        root = TreeNode(node_array.pop(0))
+        queue = [root]
+
         while queue:
-            x = queue.pop(0)
-            n = TreeNode(x)
-            if root is None:
-                root = n
+            n = queue.pop(0)
             if any(node_array):
                 l = node_array.pop(0)
-                if l == "":
+                if l == "#":
                     n.left = None
                 else:
                     n.left = TreeNode(l)
-                queue.append(l)
+                    queue.append(n.left)
             if any(node_array):
                 r = node_array.pop(0)
-                if r == "":
+                if r == "#":
                     n.right = None
                 else:
                     n.right = TreeNode(r)
-                queue.append(r)
+                    queue.append(n.right)
         return root
 
 #test cases
 s = Codec()
 t1 = s.serialize(None)
-print (t1)
 assert s.serialize(t1) == ""
 
-node1 = TreeNode(1)
-node1.left = TreeNode(2)
-node1.right = TreeNode(3)
-print (s.serialize(node1))
+node1 = TreeNode(1,TreeNode(2),TreeNode(3))
 assert s.serialize(node1) == "1,2,3"
 res1 = s.deserialize(s.serialize(node1))
-assert res1.val == "1"
-assert res1.left.val == "2"
-assert res1.right.val == "3"
+assert res1.val == "1"and res1.left.val == "2" and res1.right.val == "3"
 
-node2 = TreeNode (1)
-node2.left = TreeNode(2)
-node2.right = TreeNode(3)
-node2.right.left = TreeNode(4)
-node2.right.right = TreeNode(5)
-print (s.serialize(node2))
+node2 = TreeNode (1,TreeNode(2),TreeNode(3,TreeNode(4),TreeNode(5)))
 assert s.serialize(node2) == "1,2,3,#,#,4,5"
 res2 = s.deserialize(s.serialize(node2))
-assert res2.val == 1
-##TODO: remaining
+assert res2.val == "1" and res2.left.val == "2" and res2.right.val == "3" and res2.right.left.val == "4" and res2.right.right.val == "5"
+assert s.serialize(res2) == "1,2,3,#,#,4,5"
 
+res3 = s.deserialize("1,2")
+assert res3.val == "1" and res3.left.val == "2" and res3.right is None
+
+node4 = TreeNode(1, None, TreeNode(2))
+assert s.serialize(node4) == "1,#,2"
+
+node5 = TreeNode(1, TreeNode(3,TreeNode(4),TreeNode(5)), TreeNode(2))
+assert s.serialize(s.deserialize(s.serialize(node5))) == s.serialize(node5)
+
+node6 = TreeNode(1, TreeNode(2, None, TreeNode(4)), TreeNode(5, TreeNode(10), None))
+assert s.serialize(s.deserialize(s.serialize(node6))) == s.serialize(node6)
