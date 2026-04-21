66cfdabb8704e9c0c6f752a57d028d65bc412e3d
KS <khyati.a.shah@gmail.com>
2026-04-21 12:05:15 -0700

    solved in O(nlogn), car fleet

--- leetcode_853_carfleet.py
+++ leetcode_853_carfleet.py
@@ -1,32 +1,23 @@
 #https://leetcode.com/problems/car-fleet/description/
-import heapq
 class Solution:
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
         if len(position) == 1:
             return 1
-        def willMeet (c1, c2):
-            #the car behind is ALWAYS c1, other is c2. Can make these assumptions because local function
-            if (c1[0] < c2[0]) and (c1[1] > c2[1]):
-                time_to_target = (target - c2[0]) / c2[1]
-                time_to_meet = (c1[0]-c2[0]) / (c2[1]-c1[1])
-                return time_to_meet <= time_to_target
-            return False
-        ##NOTE: zip does not directly return list
-        s_and_p = list (zip (position, speed))
-        ##NOTE: consider the tuple (x,y) as a list. See the lambda function
-        ##NOTE: * -1 to sort by descending
-        s_and_p.sort(key = lambda x: x[0] * -1)
         n = len(position)
-        marked_positions = [0] * n
-        clusters = 0
+        ##NOTE: Here we cannot simply sort the position array because each element is coupled with one in the speed array.
+        ## We can create a joint tuple structure but that's additional memory
+        ## instead just stored the indices of the array if the sorting were to happen
+        sorted_idx = sorted (range(n), key= lambda i: position[i])
 
-        for i in range(n):
-            if marked_positions[i] == 0:
-                clusters = clusters + 1
-                marked_positions[i] = 1
-                for j in range (i+1, n):
-                    if willMeet (s_and_p[j], s_and_p[i]) and marked_positions[j] == 0:
-                        marked_positions[j] = 1
+        clusters = 0
+        prev_blocking_time = 0
+        ## We need to access in descending order, closest to target to furthest. See the pattern [::1] start from end and step ny -1
+        for i in sorted_idx[::-1]:
+            t = (target - position[i]) / speed[i]
+            if t > prev_blocking_time:
+                clusters += 1
+                prev_blocking_time = t
+
         return clusters
 
 s = Solution()
